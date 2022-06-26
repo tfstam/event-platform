2f9be9db038fa5b6f42e769b0c02b6fc8e39eee0
Tamires Santos <tfs.tamsantos@gmail.com>
2022-06-26 15:51:18 -0300

    Day 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ptqzat2ezj01xk55gh83qz/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
